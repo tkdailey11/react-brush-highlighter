7bbb57d174e53ee547d169819d0ebc8a3eeb9243
Tyler Dailey <tkdailey11@gmail.com>
2024-03-11 22:13:49 -0600

    Initial Commit

---
+++ src/brushes/index.tsx
@@ -0,0 +1,8 @@
+export { brush1 } from "./brush1";
+export { brush2 } from "./brush2";
+export { brush3 } from "./brush3";
+export { brush4 } from "./brush4";
+export { brush5 } from "./brush5";
+export { brush6 } from "./brush6";
+export { brush7 } from "./brush7";
+export { brush8 } from "./brush8";
---
+++ src/index.tsx
@@ -0,0 +1,81 @@
+import React, { PropsWithChildren, useLayoutEffect, useRef } from "react";
+import {
+  brush1,
+  brush2,
+  brush3,
+  brush4,
+  brush5,
+  brush6,
+  brush7,
+  brush8,
+} from "./brushes";
+
+interface IBrushProps {
+  style?: React.CSSProperties;
+  type: number;
+  color: string;
+}
+
+export const Brush = (props: PropsWithChildren<IBrushProps>) => {
+  const ref = useRef<SVGSVGElement>(null);
+
+  useLayoutEffect(() => {
+    const svg = ref.current;
+    if (svg) {
+      const bbox = svg.getBBox();
+      const viewBox = [bbox.x, bbox.y, bbox.width, bbox.height].join(" ");
+      svg.setAttribute("viewBox", viewBox);
+    }
+  }, []);
+
+  function renderBrush(type: number, color: string) {
+    switch (type) {
+      case 1:
+        return brush1(color);
+      case 2:
+        return brush2(color);
+      case 3:
+        return brush3(color);
+      case 4:
+        return brush4(color);
+      case 5:
+        return brush5(color);
+      case 6:
+        return brush6(color);
+      case 7:
+        return brush7(color);
+      case 8:
+        return brush8(color);
+      default:
+        return brush1(color);
+    }
+  }
+
+  return (
+    <span
+      style={{
+        position: "relative",
+        padding: "7px 58px",
+        ...(props.style || {}),
+      }}
+    >
+      <svg
+        ref={ref}
+        id="svg-brush"
+        xmlns="http://www.w3.org/2000/svg"
+        version="1.1"
+        x="0px"
+        y="0px"
+        viewBox="0 0 1100 750"
+        style={{ position: "absolute", inset: 0, zIndex: -1 }}
+        width="auto"
+        height="auto"
+        enableBackground="new 0 0 1100 500"
+        preserveAspectRatio="none"
+      >
+        {renderBrush(props.type, props.color)}
+      </svg>
+      {props.children}
+    </span>
+  );
+};
